4dfe9b77946143d31aac3ed1291e401a53ac1e9a
Neel Ratan Guria <neelratan@live.com>
2021-07-02 18:39:15 +0530

    Create match_name.py

---
+++ match_name.py
@@ -0,0 +1,26 @@
+name =input('please enter your name:')
+fruits = ["apple", "banana", "cherry"]
+
+fruits.append('Dragon fruit')
+
+matched = False
+
+for fruit in fruits:
+    if name == fruit:
+        matched = True
+
+if matched:
+    print("We got a match")
+else:
+    print("We didn't get a match")
+
+if matched:
+    print("yes")
+
+'''
+if name==x[0]:
+    print ("prasun khani master")
+else :
+    print ("bimal is best")
+    '''
+    
