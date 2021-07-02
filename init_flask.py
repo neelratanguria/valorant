8f8a187376b80f005023d3c2b506ecdeafdfb05b
Neel Ratan Guria <neelratan@live.com>
2021-07-02 20:14:28 +0530

    fridge application

--- init_flask.py
+++ init_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -25,6 +25,30 @@ def index3(name):
         Output = "Hell yeah We didn't get a match"
     
     return Output
+
+@app.route('/fridge', methods=['GET', 'POST'])
+def appFridge():
+    if request.method == 'GET':
+        return render_template('fridge_app.html')
+    if request.method == 'POST':
+        # Get value from user submission (ie from html html template using key faal)
+        faal = request.form['faal']
+        # Lets define our list again
+        fruits = ["apple", "banana", "cherry"]
+        # boolean to check if we matched the fruit
+        matched = False
+        # Iterate through fruits list and check if faal matches any fruit in list
+        for fruit in fruits:
+            if faal == fruit:
+                # Change boolean to true when the faal matches any fruit in list
+                matched = True
+        # Initialize Output variable
+        Output = ""
+        if matched:
+            Output = "Hell yeah"
+        else:
+            Output = "Hell yeah We didn't get a match"
+        return render_template('results.html',  Output=Output)
 
 if __name__ == '__main__':
    app.run()
