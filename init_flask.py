fe4a79c5f2eac1a0aee245f025a730aed3ea3cb0
Neel Ratan Guria <neelratan@live.com>
2021-07-02 18:54:49 +0530

    Update init_flask.py

--- init_flask.py
+++ init_flask.py
@@ -10,5 +10,21 @@ def index():
 def index2():
    return '<html><body><h1>Hello Neel</h1></body></html>'
 
+@app.route('/input/<name>')
+def index3(name):
+    fruits = ["apple", "banana", "cherry"]
+    fruits.append('Dragon fruit')
+    matched = False
+    for fruit in fruits:
+        if name == fruit:
+            matched = True
+    Output = ""
+    if matched:
+        Output = "Hell yeah"
+    else:
+        Output = "Hell yeah We didn't get a match"
+    
+    return Output
+
 if __name__ == '__main__':
    app.run()
