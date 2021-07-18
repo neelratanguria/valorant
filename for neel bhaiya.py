e82d0b20080941f5a2850a9321cebd8e7536063f
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2021-07-18 15:38:14 +0530

    Update for neel bhaiya.py

--- for neel bhaiya.py
+++ for neel bhaiya.py
@@ -17,11 +17,66 @@ if name in name_allowed[0]:
 
     print(message[5:6],message[13:14],message[ 7:8]+message[ 9:10]+message[-6:-5]+message[ 17:18]+message[-4:-3]+message[28:29]+message[-6:-5])
     
+    
+    
+    print ("now we gonna play a game. ")
+    
+    
+    
+    
+    def game():
+        import random
+        
+        x=[random.randint(0,3) for i in range(4)]
+        print(x)
+        if x == [3,3,3,3]:
+            print('win')
+        else :
+            print('lose')
+            
+    
+    command=""
+    
+    started=False
+    
+    while True:
+        command = input(">>>  ").lower()
+        if command == "start":
+             game()
+             
+       
+        elif command =="try again":
+            
+            if not started:
+                
+                game()
+            else :
+                print('start the game first')
+            
+             
+        
+        elif command == "help":
+            print('''
+                  "start" = "to start"
+                  "try again" = "to try again"
+                  "quit" = "to exit"
+                  
+                  if all numbers are same you win 
+                  good luck
+                 
+                 '''         )
+    
+        elif command =="quit":
+            break
+        
+        else:
+            print("sorry i don't understand that")
 elif name in name_allowed[1]:
     print(name + " made this one for u")
     
 elif name in name_allowed[2]:
     print(name + " is a friend of your")
+    
     
 elif name in name_allowed[3]:
     print(name + " is a friend of mine")
@@ -34,57 +89,5 @@ else :
     
 
 
-print ("now we gonna play a game. ")
-
-
-
-
-def game():
-    import random
-    
-    x=[random.randint(0,3) for i in range(4)]
-    print(x)
-    if x == [3,3,3,3]:
-        print('win')
-    else :
-        print('lose')
-        
-
-command=""
-
-started=False
-
-while True:
-    command = input(">>>  ").lower()
-    if command == "start":
-         game()
-         
-   
-    elif command =="try again":
-        
-        if not started:
-            
-            game()
-        else :
-            print('start the game first')
-        
-         
-    
-    elif command == "help":
-        print('''
-              "start" = "to start"
-              "try again" = "to try again"
-              "quit" = "to exit"
-              
-              if all numbers are same you win 
-              good luck
-             
-             '''         )
-
-    elif command =="quit":
-        break
-    
-    else:
-        print("sorry i don't understand that")
 
 
